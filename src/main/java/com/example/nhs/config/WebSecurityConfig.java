618058a420f4e4559bcad068e48ac5223f1931d7
heeseok <nhs0912@nate.com>
2023-06-12 12:24:37 +0900

    feat : filterChain method 구현

--- src/main/java/com/example/nhs/config/WebSecurityConfig.java
+++ src/main/java/com/example/nhs/config/WebSecurityConfig.java
@@ -4,7 +4,9 @@ import com.example.nhs.member.service.MemberDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
@@ -21,6 +23,25 @@ public class WebSecurityConfig {
                 .requestMatchers("/static/**");
     }
 
-    
+    //특정 HTTP 요청에 대한 웹 기반 보안 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()// 인증, 인가 설정
+                .requestMatchers("/login", "/signup", "/user").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin() // 폼 기반 로그인 설정
+                .loginPage("/login")
+                .defaultSuccessUrl("/articles")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .and()
+                .csrf().disable()
+                .build();
+    }
+
 
 }
