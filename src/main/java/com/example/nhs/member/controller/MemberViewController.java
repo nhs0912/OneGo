486eb41f7f1b3bd0a69b32189e3b522fabe1ccb3
heeseok <nhs0912@nate.com>
2023-06-12 13:15:13 +0900

    feat : signup, login view controller and html file 추가

---
+++ src/main/java/com/example/nhs/member/controller/MemberViewController.java
@@ -0,0 +1,18 @@
+package com.example.nhs.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup(){
+        return "signup";
+    }
+}
