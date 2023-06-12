013eb4bbf24c24f26755cc6b74eee99d640099d3
heeseok <nhs0912@nate.com>
2023-06-12 12:27:37 +0900

    feat : authenticationManager 구현

--- src/main/java/com/example/nhs/config/WebSecurityConfig.java
+++ src/main/java/com/example/nhs/config/WebSecurityConfig.java
@@ -4,8 +4,11 @@ import com.example.nhs.member.service.MemberDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
@@ -43,5 +46,15 @@ public class WebSecurityConfig {
                 .build();
     }
 
+    //인증 관리자 관련 설정
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, MemberDetailService memberDetailService) throws Exception{
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(memberService) //사용자 정보 서비스 설정
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+
+    }
 
 }
