7846a0d3b0e4892b8aa6063270985b384a89e994
heeseok <nhs0912@nate.com>
2023-09-10 10:47:06 +0900

    test : memberService test find와 save하는 부분 테스트 완료

--- src/main/java/com/example/nhs/member/service/dto/AddMemberRequest.java
+++ src/main/java/com/example/nhs/member/service/dto/AddMemberRequest.java
@@ -1,19 +1,20 @@
 package com.example.nhs.member.service.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 public class AddMemberRequest {
     private String employeeId;
     private String password;
     private String name;
     private String memberRole;
     private Long teamId;
-
     @Override
     public String toString() {
         return "employeeId = " + employeeId + " , " + "password = " + password + " , name= " + name + " ,memberRole= " + memberRole +" ,teamId= " + teamId;
     }
+
 }
