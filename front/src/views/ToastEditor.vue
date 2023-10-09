37b9aad82ce54c5b88aab143163db27decb21c90
heeseok.noh <nhs0912@nate.com>
2023-10-09 22:51:24 +0900

    feat : toast editor plugin 적용

--- front/src/views/ToastEditor.vue
+++ front/src/views/ToastEditor.vue
@@ -8,7 +8,17 @@ import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import Editor from '@toast-ui/editor';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
+import '@toast-ui/chart/dist/toastui-chart.css';
 
+import chart from '@toast-ui/editor-plugin-chart';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import '@toast-ui/editor-plugin-table-merged-cell/dist/toastui-editor-plugin-table-merged-cell.css';
+
+import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
+import uml from '@toast-ui/editor-plugin-uml';
 export default {
   data() {
     return {
@@ -22,11 +32,10 @@ export default {
       height: '500px',
       initialEditType: 'markdown',
       previewStyle: 'vertical',
-      plugins: [codeSyntaxHighlight]
+      plugins: [codeSyntaxHighlight, chart, colorSyntax, tableMergedCell, uml]
     });
   }
 }
 
 
 </script>
-
