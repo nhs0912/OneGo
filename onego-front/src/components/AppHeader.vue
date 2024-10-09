3040d29507985b000484fd0ec34603b712e9d9e5
david Noh <nhs0912@nate.com>
2024-10-09 20:29:17 +0900

    feat : vue first screen 완성

---
+++ onego-front/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {useTheme} from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <v-toolbar flat>
+    <v-toolbar-title>OneGo</v-toolbar-title>
+    <v-spacer></v-spacer>
+      <!-- button uses dark theme -->
+      <v-btn icon id="mode-switcher" @click="toggleTheme">
+        <v-icon :color="($vuetify.theme.dark) ? 'primary' : 'primary lighten-4'">
+          {{ (theme.global.current.value.dark) ? 'mdi-weather-night' : 'mdi-weather-sunny' }}
+        </v-icon>
+
+      </v-btn>
+  </v-toolbar>
+
+</template>
+
+<style scoped lang="sass">
+
+</style>
---
+++ onego-front/src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <LoginView />
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
