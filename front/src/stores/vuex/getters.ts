da151366eeaf724b991bea2295acf9296ba2e10c
david Noh <nhs0912@nate.com>
2024-10-09 17:27:34 +0900

    feat : front 폴더 삭제

--- front/src/stores/vuex/getters.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  getUserId: state => state.userId,
-  getErrorState: state => state.errorState,
-  getIsAuth: state => state.isAuth,
-  loggedIn(state) {
-    return !!state.user
-  }
-}
