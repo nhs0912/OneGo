1aacd78f51dd33257d0482e31c416413928b8a41
nhs0912 <nhs0912@nate.com>
2024-11-26 17:43:24 +0900

    fix : remove a image at login and signup view

--- onego-front/src/components/LoginView.vue
+++ onego-front/src/components/LoginView.vue
@@ -1,78 +1,78 @@
 <template>
   <div>
-    <v-img
-        class="mx-auto my-6"
-        max-width="228"
-        src="@/assets/image/onego_logo.png"
-    ></v-img>
-<!--    :src="require(`@/assets/image/vuetify_logo.svg`)"-->
+    <!-- <v-img
+      class="mx-auto my-6"
+      max-width="228"
+      src="@/assets/image/onego_logo.png"
+    /> -->
+    <!--    :src="require(`@/assets/image/vuetify_logo.svg`)"-->
     <v-card
-        class="mx-auto pa-12 pb-8"
-        elevation="8"
-        max-width="448"
-        rounded="lg"
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
     >
       <div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
       <v-text-field
-          density="compact"
-          placeholder="Email address"
-          prepend-inner-icon="mdi-email-outline"
-          variant="outlined"
-      ></v-text-field>
+        density="compact"
+        placeholder="Email address"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"
+      />
 
       <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
         Password
         <a
-            class="text-caption text-decoration-none text-blue"
-            href="#"
-            rel="noopener noreferrer"
-            target="_blank"
+          class="text-caption text-decoration-none text-blue"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
         >
           Forgot login password?</a>
       </div>
 
       <v-text-field
-          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-          :type="visible ? 'text' : 'password'"
-          density="compact"
-          placeholder="Enter your password"
-          prepend-inner-icon="mdi-lock-outline"
-          variant="outlined"
-          @click:append-inner="visible = !visible"
-      ></v-text-field>
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        density="compact"
+        placeholder="Enter your password"
+        prepend-inner-icon="mdi-lock-outline"
+        :type="visible ? 'text' : 'password'"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      />
 
       <v-card
-          class="mb-12"
-          color="surface-variant"
-          variant="tonal"
+        class="mb-12"
+        color="surface-variant"
+        variant="tonal"
       >
         <!--        <v-card-text class="text-medium-emphasis text-caption">-->
         <!--          Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.-->
         <!--        </v-card-text>-->
       </v-card>
-      <router-link to='/mainView'>
-      <v-btn
-          :loading="loading"
+      <router-link to="/mainView">
+        <v-btn
+          block
           class="mb-8"
           color="blue"
+          :loading="loading"
           size="large"
           variant="tonal"
-          block
-          @click="this.isLogin = !this.isLogin"
-      >
-        Log In
-      </v-btn>
+          @click="isLogin = !isLogin"
+        >
+          Log In
+        </v-btn>
       </router-link>
 
       <v-card-text class="text-center">
-        <router-link to='/login/signupView'>
+        <router-link to="/login/signupView">
           <a
-              class="text-blue text-decoration-none"
-              rel="noopener noreferrer"
+            class="text-blue text-decoration-none"
+            rel="noopener noreferrer"
           >
             Sign up now
-            <v-icon icon="mdi-chevron-right"></v-icon>
+            <v-icon icon="mdi-chevron-right" />
           </a>
         </router-link>
       </v-card-text>
@@ -83,14 +83,14 @@
 // import {ref, computed} from 'vue';
 // import {useStore} from 'vuex';
 
-import {useAppStore} from "@/stores/app";
+  import { useAppStore } from '@/stores/app'
 
-let store = useAppStore();
+  const store = useAppStore()
 
-export default {
-  data: () => ({
-    visible: true,
-    isLogin : store.isLogin
-  }),
-}
+  export default {
+    data: () => ({
+      visible: true,
+      isLogin: store.isLogin,
+    }),
+  }
 </script>
--- onego-front/src/pages/login/signupView.vue
+++ onego-front/src/pages/login/signupView.vue
@@ -1,138 +1,140 @@
 <template>
   <div>
-  <form>
-    <v-img
-      class="mx-auto my-6"
-      max-width="228"
-      src="@/assets/image/onego_logo.png"
-    ></v-img>
-    <v-card
-      class="mx-auto pa-12 pb-10"
-      elevation="10"
-      max-width="448"
-      rounded="lg"
-    >
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-
-      <v-text-field
-        v-model="state.email"
-        :type="visible ? 'text' : 'account'"
-        :error-messages="v$.email.$errors.map(e => e.$message)"
-        density="compact"
-        placeholder="Email address"
-        prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
-        @blur="v$.email.$touch"
-        @input="v$.email.$touch"
-      ></v-text-field>
-
-      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-        Password              </div>
-      <v-text-field
-        v-model="state.password"
-        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
-        :error-messages="v$.password.$errors.map(e => e.$message)"
-        density="compact"
-        placeholder="Enter your password"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
-        @click:append-inner="visible = !visible"
-        @blur="v$.password.$touch"
-        @input="v$.password.$touch"
-      ></v-text-field>
-      <div class="text-subtitle-1 text-medium-emphasis">Name</div>
-      <v-text-field
-        v-model="state.name"
-        :counter="10"
-        :error-messages="v$.name.$errors.map(e => e.$message)"
-        density="compact" placeholder="Enter your name"
-        prepend-inner-icon="mdi-pencil-outline"
-        variant="outlined"
-        label="Name"
-        required
-        @blur="v$.name.$touch"
-        @input="v$.name.$touch"
-      ></v-text-field>
-      <div class="text-subtitle-1 text-medium-emphasis">Team</div>
-      <v-select
-        v-model="state.select"
-        :error-messages="v$.select.$errors.map(e => e.$message)"
-        :items="teams"
-        label="Team"
-        density="compact" placeholder="Enter your Team"
-        prepend-inner-icon="mdi-pencil-outline"
-        variant="outlined"
-        required
-        @blur="v$.select.$touch"
-        @change="v$.select.$touch"
-      ></v-select>
-
-      <v-checkbox
-        v-model="state.checkbox"
-        :error-messages="v$.checkbox.$errors.map(e => e.$message)"
-        label="Do you agree?"
-        required
-        @blur="v$.checkbox.$touch"
-        @change="v$.checkbox.$touch"
-      ></v-checkbox>
-      <v-btn
-        class="me-4"
-        @click="v$.$validate"
+    <form>
+      <!-- <v-img
+        class="mx-auto my-6"
+        max-width="228"
+        src="@/assets/image/onego_logo.png"
+      /> -->
+      <v-card
+        class="mx-auto pa-12 pb-10"
+        elevation="10"
+        max-width="448"
+        rounded="lg"
       >
-        submit
-      </v-btn>
-      <v-btn @click="$router.go(-1)">
-        back
-      </v-btn>
-    </v-card>
-  </form>
+        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+        <v-text-field
+          v-model="state.email"
+          density="compact"
+          :error-messages="v$.email.$errors.map(e => e.$message)"
+          placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline"
+          :type="visible ? 'text' : 'account'"
+          variant="outlined"
+          @blur="v$.email.$touch"
+          @input="v$.email.$touch"
+        />
+
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          Password              </div>
+        <v-text-field
+          v-model="state.password"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          density="compact"
+          :error-messages="v$.password.$errors.map(e => e.$message)"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          :type="visible ? 'text' : 'password'"
+          variant="outlined"
+          @blur="v$.password.$touch"
+          @click:append-inner="visible = !visible"
+          @input="v$.password.$touch"
+        />
+        <div class="text-subtitle-1 text-medium-emphasis">Name</div>
+        <v-text-field
+          v-model="state.name"
+          :counter="10"
+          density="compact"
+          :error-messages="v$.name.$errors.map(e => e.$message)"
+          label="Name"
+          placeholder="Enter your name"
+          prepend-inner-icon="mdi-pencil-outline"
+          required
+          variant="outlined"
+          @blur="v$.name.$touch"
+          @input="v$.name.$touch"
+        />
+        <div class="text-subtitle-1 text-medium-emphasis">Team</div>
+        <v-select
+          v-model="state.select"
+          density="compact"
+          :error-messages="v$.select.$errors.map(e => e.$message)"
+          :items="teams"
+          label="Team"
+          placeholder="Enter your Team"
+          prepend-inner-icon="mdi-pencil-outline"
+          required
+          variant="outlined"
+          @blur="v$.select.$touch"
+          @change="v$.select.$touch"
+        />
+
+        <v-checkbox
+          v-model="state.checkbox"
+          :error-messages="v$.checkbox.$errors.map(e => e.$message)"
+          label="Do you agree?"
+          required
+          @blur="v$.checkbox.$touch"
+          @change="v$.checkbox.$touch"
+        />
+        <v-btn
+          class="me-4"
+          @click="v$.$validate"
+        >
+          submit
+        </v-btn>
+        <v-btn @click="$router.go(-1)">
+          back
+        </v-btn>
+      </v-card>
+    </form>
   </div>
 </template>
 <script setup>
-import { reactive } from 'vue'
-import { useVuelidate } from '@vuelidate/core'
-import { email, required } from '@vuelidate/validators'
+  import { reactive } from 'vue'
+  import { useVuelidate } from '@vuelidate/core'
+  import { email, required } from '@vuelidate/validators'
 
-const initialState = {
-  name: '',
-  email: '',
-  select: null,
-  checkbox: null,
-  password: '',
-}
-
-const state = reactive({
-  ...initialState,
-})
-
-const teams = [
-  'FO',
-  'BO',
-  'Common',
-  'etc',
-]
-
-const rules = {
-  name: { required },
-  email: { required, email },
-  select: { required },
-  teams: { required },
-  checkbox: { required },
-  password: { required },
-}
-
-const v$ = useVuelidate(rules, state)
-
-function clear () {
-  v$.value.$reset()
-
-  for (const [key, value] of Object.entries(initialState)) {
-    state[key] = value
+  const initialState = {
+    name: '',
+    email: '',
+    select: null,
+    checkbox: null,
+    password: '',
   }
-}
 
-function back() {
-  router.go(-1);
-}
+  const state = reactive({
+    ...initialState,
+  })
+
+  const teams = [
+    'FO',
+    'BO',
+    'Common',
+    'etc',
+  ]
+
+  const rules = {
+    name: { required },
+    email: { required, email },
+    select: { required },
+    teams: { required },
+    checkbox: { required },
+    password: { required },
+  }
+
+  const v$ = useVuelidate(rules, state)
+
+  function clear () {
+    v$.value.$reset()
+
+    for (const [key, value] of Object.entries(initialState)) {
+      state[key] = value
+    }
+  }
+
+  function back () {
+    router.go(-1)
+  }
 </script>
